5cb52561cce39e14a7c013688ced071dd7f57065
cks-wls <slo0521@naver.com>
2025-09-03 14:49:33 +0900

    feat : 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createGlobalStyle } from "styled-components";
+import { Routes, Route } from "react-router-dom";
+import Main from "@/pages/main";
+import Layout from "@/pages/layout";
+import MovieDetail from "@/components/main/MovieDetail";
+import Latout from "./pages/Layout";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Latout />}>
+          <Route index element={<Main />} />
+          <Route path="/movies/:movieId" element={<MovieDetail />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+const GlobalStyle = createGlobalStyle`
+ * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`;
