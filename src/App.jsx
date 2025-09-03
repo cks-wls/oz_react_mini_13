6f02474771b82608e0606b07e526b33d45e2cd6c
cks-wls <slo0521@naver.com>
2025-09-03 15:37:38 +0900

    fix : 오타 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Main from "@/pages/main";
 import Layout from "@/pages/layout";
 import MovieDetail from "@/components/main/MovieDetail";
-import Latout from "./pages/Layout";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Latout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Main />} />
           <Route path="/movies/:movieId" element={<MovieDetail />} />
         </Route>
