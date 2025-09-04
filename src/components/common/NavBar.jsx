8e9a22fc3057bbd7682d592b160da0ca033e2776
cks-wls <slo0521@naver.com>
2025-09-04 11:49:28 +0900

    feat : 추후 모드를 위한 버튼 구현

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,12 +1,24 @@
 import styled from "styled-components";
+import moon from "@/assets/icons/moon.png";
+import sun from "@/assets/icons/sun.png";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 function NavBar() {
   const navigate = useNavigate();
+  const [mode, setMode] = useState(true);
   return (
     <Container>
       <Title onClick={() => navigate("/")}>OZ MOVIE</Title>
-      <Search placeholder="   검색어를 입력해주세요" />
+      <Search placeholder="검색어를 입력해주세요" />
       <LoginContainer>
+        <Mode
+          onClick={() => {
+            setMode(!mode);
+          }}
+          $color={mode ? "white" : "black"}
+        >
+          <Img src={mode ? moon : sun} alt="modeIcon" />
+        </Mode>
         <Button>로그인</Button>
         <Button>회원가입</Button>
       </LoginContainer>
@@ -29,10 +41,25 @@ const Title = styled.h1`
 const Search = styled.input`
   width: 300px;
   border-radius: 5px;
+  padding-left: 20px;
 `;
 const LoginContainer = styled.div`
   display: flex;
   gap: 20px;
+`;
+const Mode = styled.button`
+  background-color: ${(props) => props.$color};
+  padding: 8px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid white;
+  margin-right: 10px;
+`;
+const Img = styled.img`
+  width: 20px;
+  height: 20px;
 `;
 const Button = styled.button`
   padding: 0.5rem 0.5rem;
