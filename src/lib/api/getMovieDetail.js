448a0e9dbbe2c35f79703fe25a6404374d60b462
cks-wls <slo0521@naver.com>
2025-09-04 18:31:10 +0900

    feat : API 수정

--- src/lib/api/getMovieDetail.js
+++ src/lib/api/getMovieDetail.js
@@ -1,17 +1,29 @@
 export default async function getMovieList() {
   try {
-    const response = await fetch("/data/movieDetailData.json");
+    const response = await fetch("/data/movieListData.json");
     const data = await response.json();
-    const id = Number(data.id);
-    const path = data.poster_path;
-    const title = data.title;
-    const average = data.vote_average;
-    const overView = data.overview;
-    const genres = data.genres.map((val) => val.name);
-    const details = { id, path, title, average, overView, genres };
+    const details = data.results.map(
+      ({
+        id,
+        title,
+        poster_path,
+        overview,
+        vote_average,
+        genre_ids,
+        release_date,
+      }) => ({
+        id,
+        title,
+        poster_path,
+        overview,
+        vote_average,
+        genre_ids,
+        release_date,
+      })
+    );
     return details;
   } catch {
     return [];
   }
 }
-//  영화 정보를 받아오는 API -> 쿨푸팬더에 한함
+//  영화 정보를 받아오는 API
--- src/lib/api/getMovieList.js
+++ src/lib/api/getMovieList.js
@@ -3,14 +3,15 @@ export default async function getMovieList() {
     const response = await fetch("/data/movieListData.json");
     const data = await response.json();
 
-    const movies = data.results.map(
-      ({ id, title, vote_average, poster_path }) => ({
+    const movies = data.results
+      .filter((val) => val.adult === false)
+      // 성인영화가 아닌것만 가져오게 설정
+      .map(({ id, title, vote_average, poster_path }) => ({
         id,
         title,
         vote_average,
         poster_path,
-      })
-    );
+      }));
     return movies;
   } catch {
     return [];
