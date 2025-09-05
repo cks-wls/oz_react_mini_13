ef9fd6b7b948a08e736eae5aa0e067edb60e7738
cks-wls <slo0521@naver.com>
2025-09-05 10:33:45 +0900

    feat : 더미데이터가 아닌 API로 불러올 수 있게 수정

--- src/lib/api/getMovieDetail.js
+++ src/lib/api/getMovieDetail.js
@@ -1,6 +1,16 @@
+const TMDB_ACCESS_TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
 export default async function getMovieList() {
   try {
-    const response = await fetch("/data/movieListData.json");
+    const options = {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+      },
+    };
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
+      options
+    );
     const data = await response.json();
     const details = data.results.map(
       ({
