adfce4be22a7e0ea78aaa24fda14cf9e27b81a8e
cks-wls <slo0521@naver.com>
2025-09-08 11:48:07 +0900

    feat : 검색 모달 구현

---
+++ src/components/modal/SearchModal.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+function SearchModal({ mode }) {
+  return (
+    <Container $bgcolor={mode === "light" ? "black" : "#1c1c1c"}>
+      <Search placeholder="검색어를 입력해주세요" />
+    </Container>
+  );
+}
+
+export default SearchModal;
+
+const tabletWidth = "768px";
+const Container = styled.div`
+  padding: 20px 0;
+  background-color: ${(props) => props.$bgcolor};
+  display: flex;
+  animation: slideDown 0.5s ease;
+  @keyframes slideDown {
+    from {
+      opacity: 0;
+      transform: translateY(-10px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+  @media screen and (min-width: ${tabletWidth}) {
+    display: none;
+  }
+  @media screen and (min-width: ${tabletWidth}) {
+    display: none;
+  }
+`;
+const Search = styled.input`
+  width: 90%;
+  margin: 0 auto;
+  border-radius: 8px;
+  padding: 10px 0 10px 20px;
+`;
