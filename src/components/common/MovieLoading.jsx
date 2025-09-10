04c577d01cfb760b17a16d1e4c304d010231b702
cks-wls <slo0521@naver.com>
2025-09-10 16:08:21 +0900

    feat : 모드에 따라 글씨 색 바뀌게 구현

--- src/components/common/MovieLoading.jsx
+++ src/components/common/MovieLoading.jsx
@@ -1,10 +1,18 @@
 import styled from "styled-components";
+import ModeContext from "@/context/ModeContext";
+import { useContext } from "react";
 function MovieLoading() {
-  return <Container>영화 목록 불러오는 중... ⏳</Container>;
+  const { mode } = useContext(ModeContext);
+  return (
+    <Container $color={mode === "light" ? "black" : "white"}>
+      영화 목록 불러오는 중... ⏳
+    </Container>
+  );
 }
 export default MovieLoading;
 const Container = styled.div`
   display: flex;
   justify-content: center;
   padding-bottom: 40px;
+  color: ${(props) => props.$color};
 `;
